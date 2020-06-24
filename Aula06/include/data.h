d48d8c17f5584d00a140453d2b7bb3749ad2455c
Lucas Almeida <48758566+lucas-sc@users.noreply.github.com>
2020-06-24 12:06:32 -0300

    Criando data.h

---
+++ Aula06/include/data.h
@@ -0,0 +1,17 @@
+#ifndef DATA.H
+#define DATA.H
+#include <string>
+
+struct Horario {
+  int hora;
+  int minuto;
+  int segundo;
+};
+
+struct Data {
+  int dia;
+  int mes;
+  int ano;  
+};
+
+#endif
