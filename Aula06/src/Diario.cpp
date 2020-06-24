acfae4bbdf802b1aa7c5136ba54f3f5b5ba7f0c1
Lucas Almeida <48758566+lucas-sc@users.noreply.github.com>
2020-06-24 16:00:08 -0300

    Atualizando Diario.cpp

---
+++ Aula06/src/Diario.cpp
@@ -0,0 +1,11 @@
+#include "Diario.h"
+
+ Diario::Diario(const std::string& nome_arquivo): nome_arquivo(nome_arquivo) {
+
+ }
+ void Diario::add(const std::string& mensagem){
+      // adicionar mensagem no array de mensagens
+ }
+ void Diario::escreveArquivo(){
+     // gravar as mensagens no disco
+ }
