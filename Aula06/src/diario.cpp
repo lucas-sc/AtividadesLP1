dda8dc4fc413b2a6c0ad7f8a7d16ce41773e9680
Lucas Almeida <48758566+lucas-sc@users.noreply.github.com>
2020-06-24 12:08:57 -0300

    Criando diario.cpp

---
+++ Aula06/src/diario.cpp
@@ -0,0 +1,12 @@
+#include <diario.h>
+
+using namespace std;
+
+string Diario solicitaMensagem(){
+};
+int Diario::contadorDatas (string data) {
+};
+int Diario::escreveArquivo(int contador, string data, string hora, string mensagem){
+};
+int Diario::listaMensagem(string mensagem) {
+};
