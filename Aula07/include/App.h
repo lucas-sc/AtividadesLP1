53494d982a0a9f5de40b20911dddef4cd5269f42
Lucas Almeida <48758566+lucas-sc@users.noreply.github.com>
2020-06-24 16:30:50 -0300

    App.h da versão anterior

---
+++ Aula07/include/App.h
@@ -0,0 +1,19 @@
+#ifndef APP_H
+#define APP_H
+
+#include "Diario.h"
+#include <string>
+
+struct App {
+    Diario diario;
+    App(const std::string& nome_arquivo); //construtor. Deve ser com o mesmo nome da struct
+    int run(int argc, char* argv[]); /*funcao padrao similar a funcao main;
+                                     Recebe os argumentos e retorna um inteiro para 
+                                     indicar se o programa rodou corretamente ou não*/
+    int uso();
+    void add();
+    void add(const std::string mensagem);
+    void lista_mensagens();
+    
+};
+#endif
